d05f4a102b2168cb114d282d897ab592122f3d7a
AnishRane <anish.rane21@outlook.com>
2025-10-15 10:50:49 +0530

    update: register health check module and response interceptor module in app module at global level

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HealthModule } from './health/health.module';
+import { MetricsTrackerInterceptor } from './health/metrics-tracker.interceptor';
 
 @Module({
-  imports: [],
+  imports: [HealthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: MetricsTrackerInterceptor,
+    },
+  ],
 })
 export class AppModule {}
