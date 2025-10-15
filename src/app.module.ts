c34dd76b1a16197c7fb63b0e18c25d0dcb2fd598
AnishRane <anish.rane21@outlook.com>
2025-10-15 22:55:02 +0530

    add: regisgter campaign, metrics and intent modules to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,13 @@ import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
+import { CampaignsModule } from './campaigns/campaigns.module';
+import { MetricsModule } from './metrics/metrics.module';
+import { IntentModule } from './intent/intent.module';
 import { MetricsTrackerInterceptor } from './health/metrics-tracker.interceptor';
 
 @Module({
-  imports: [HealthModule],
+  imports: [HealthModule, CampaignsModule, MetricsModule, IntentModule],
   controllers: [AppController],
   providers: [
     AppService,
